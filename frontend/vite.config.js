056d08cab43381dd1ad6466377f569ad147570d7
PrateekKumarSingh <calledasprateek@outlook.com>
2024-09-19 08:59:13 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
+});
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+}
+
+>>>>>>> origin/main
